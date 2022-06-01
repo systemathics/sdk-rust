6d9291a2e1e9eec8993ea1b7a53d0dd9126fed45
Rémy Frèrebeau <remy.frerebeau@systemathics.com>
2022-06-01 17:27:44 +0200

    cargo fmt

--- build.rs
+++ build.rs
@@ -3,17 +3,16 @@ use walkdir::{DirEntry, WalkDir};
 
 fn is_proto(entry: Option<DirEntry>) -> Option<DirEntry> {
     match entry {
-        Some(e) => 
-            match e.path().to_str() {
-                Some(name) => match name.ends_with(".proto") {
-                    true => Some(e),
-                    false => None
-                },
-                None => None
+        Some(e) => match e.path().to_str() {
+            Some(name) => match name.ends_with(".proto") {
+                true => Some(e),
+                false => None,
             },
+            None => None,
+        },
         None => None,
-        }        
     }
+}
 
 // fn display(vec: &Vec<&Path>) -> () {
 //     for element in vec {
@@ -22,23 +21,20 @@ fn is_proto(entry: Option<DirEntry>) -> Option<DirEntry> {
 // }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut vec : Vec<DirEntry> = Vec::new();
+    let mut vec: Vec<DirEntry> = Vec::new();
     for entry in WalkDir::new("proto")
         .into_iter()
         .filter_map(|e| (is_proto(e.ok())))
     {
         //println!("{}", entry.path().display());
-        vec.push(entry);        
+        vec.push(entry);
     }
     //println!("{}", vec.len());
-    let protos : Vec<&Path> =  vec.iter().map(|d| d.path()).collect();
+    let protos: Vec<&Path> = vec.iter().map(|d| d.path()).collect();
     //display(&protos);
     tonic_build::configure()
-    .build_server(false)
-    .out_dir("src")
-    .compile(
-        &protos[..],
-        &["proto"]
-    )?;
+        .build_server(false)
+        .out_dir("src")
+        .compile(&protos[..], &["proto"])?;
     return Ok(());
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-
 #[path = ""]
 pub mod google {
     #[path = "google.r#type.rs"]
@@ -15,10 +14,10 @@ pub mod systemathics {
         pub mod r#type {
 
             #[path = ""]
-            pub mod shared {   
-                        
+            pub mod shared {
+
                 #[path = "systemathics.apis.r#type.shared.v1.rs"]
-                pub mod v1;                
+                pub mod v1;
             }
         }
 
@@ -29,28 +28,28 @@ pub mod systemathics {
             pub mod corporate_actions {
 
                 #[path = "systemathics.apis.services.corporate_actions.v1.rs"]
-                pub mod v1; 
+                pub mod v1;
             }
 
             #[path = ""]
             pub mod daily {
 
                 #[path = "systemathics.apis.services.daily.v1.rs"]
-                pub mod v1; 
+                pub mod v1;
             }
 
             #[path = ""]
             pub mod daily_analytics {
 
                 #[path = "systemathics.apis.services.daily_analytics.v1.rs"]
-                pub mod v1; 
+                pub mod v1;
             }
 
             #[path = ""]
             pub mod indices {
 
                 #[path = "systemathics.apis.services.indices.v1.rs"]
-                pub mod v1; 
+                pub mod v1;
             }
 
             #[cfg(feature = "intraday")]
@@ -58,57 +57,56 @@ pub mod systemathics {
             pub mod intraday {
 
                 #[path = "systemathics.apis.services.intraday.v1.rs"]
-                pub mod v1; 
+                pub mod v1;
             }
 
             #[path = ""]
             pub mod intraday_analytics {
 
                 #[path = "systemathics.apis.services.intraday_analytics.v1.rs"]
-                pub mod v1; 
+                pub mod v1;
             }
 
             #[path = ""]
             pub mod static_data {
 
                 #[path = "systemathics.apis.services.static_data.v1.rs"]
-                pub mod v1; 
+                pub mod v1;
             }
 
             #[path = ""]
             pub mod sustainability {
 
                 #[path = "systemathics.apis.services.sustainability.v1.rs"]
-                pub mod v1; 
+                pub mod v1;
             }
 
             #[path = ""]
             pub mod tick {
 
                 #[path = "systemathics.apis.services.tick.v1.rs"]
-                pub mod v1; 
+                pub mod v1;
             }
 
             #[path = ""]
             pub mod tick_analytics {
 
                 #[path = "systemathics.apis.services.tick_analytics.v1.rs"]
-                pub mod v1; 
+                pub mod v1;
             }
 
-            
             #[path = ""]
             pub mod tick_conditions {
 
                 #[path = "systemathics.apis.services.tick_conditions.v1.rs"]
-                pub mod v1; 
+                pub mod v1;
             }
 
             #[path = ""]
             pub mod topology {
 
                 #[path = "systemathics.apis.services.topology.v1.rs"]
-                pub mod v1; 
+                pub mod v1;
             }
         }
     }
